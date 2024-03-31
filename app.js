2f013cb3a57eba969d70619cbfe4468f2417930d
timatima0314 <tima314@gmail.com>
2024-03-31 15:36:33 +0900

    add:methodの重複発動を防止するコード追加

--- app.js
+++ app.js
@@ -6,14 +6,22 @@ const slideSpeed = "3s";
 window.electronAPI.downSlide(() => {
   var rect = elem.getBoundingClientRect();
   console.log(rect.top);
-  let h = rect.top - 960;
-  elem.style.transform = `translate3d( 0px,${h}px,0px)`;
-  elem.style.transitionDuration = slideSpeed;
+  if (rect.top === 0 || rect.top === -960 || rect.top === -1920) {
+    let h = rect.top - 960;
+    elem.style.transform = `translate3d( 0px,${h}px,0px)`;
+    elem.style.transitionDuration = slideSpeed;
+  } else {
+    return;
+  }
 });
 window.electronAPI.upSlide(() => {
   var rect = elem.getBoundingClientRect();
   console.log(rect.top);
-  let h = rect.top + 960;
-  elem.style.transform = `translate3d( 0px,${h}px,0px)`;
-  elem.style.transitionDuration = slideSpeed;
+  if (rect.top === -960 || rect.top === -1920 || rect.top === -2880) {
+    let h = rect.top + 960;
+    elem.style.transform = `translate3d( 0px,${h}px,0px)`;
+    elem.style.transitionDuration = slideSpeed;
+  } else {
+    return;
+  }
 });
