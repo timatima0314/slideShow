ad35a9adbff3e7d55c09d01499f02d9c0e1c6cec
timatima0314 <tima314@gmail.com>
2024-03-31 14:35:45 +0900

    スライドダウンmethod完成

--- preload.js
+++ preload.js
@@ -1,6 +1,6 @@
 const { ipcRenderer, contextBridge } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
-  butotnClick: (callback) =>
-    ipcRenderer.on("butotn-click", (_event, value) => callback(value)),
+  downSlide: (callback) =>
+    ipcRenderer.on("down", (_event, value) => callback(value)),
 });
