d352768ec51ba476c1c2e74c46f7a7d0d1e02ca4
timatima0314 <tima314@gmail.com>
2024-03-31 14:45:52 +0900

    スライドアップmethod完成

--- app.js
+++ app.js
@@ -10,3 +10,10 @@ window.electronAPI.downSlide(() => {
   elem.style.transform = `translate3d( 0px,${h}px,0px)`;
   elem.style.transitionDuration = slideSpeed;
 });
+window.electronAPI.upSlide(() => {
+  var rect = elem.getBoundingClientRect();
+  console.log(rect.top);
+  let h = rect.top + 960;
+  elem.style.transform = `translate3d( 0px,${h}px,0px)`;
+  elem.style.transitionDuration = slideSpeed;
+});
--- preload.js
+++ preload.js
@@ -3,4 +3,6 @@ const { ipcRenderer, contextBridge } = require("electron");
 contextBridge.exposeInMainWorld("electronAPI", {
   downSlide: (callback) =>
     ipcRenderer.on("down", (_event, value) => callback(value)),
+  upSlide: (callback) =>
+    ipcRenderer.on("up", (_event, value) => callback(value)),
 });
